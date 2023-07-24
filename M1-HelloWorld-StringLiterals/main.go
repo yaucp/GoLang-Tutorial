b3760cc90dd22d0d06824aee1de1b1e9c919167f
Thomas Yau <yaucp@connect.hku.hk>
2023-07-25 00:30:12 +0800

    add module 1 hello world!!
    Hello Gophers!

---
+++ M1-HelloWorld-StringLiterals/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// Need package main and main func to run as a executable
+func main() {
+	fmt.Println("Whatsup My Gophers!")
+
+	//Backticks for multi-line (raw string literals)
+
+	fmt.Println(`Whatsup Hello!!
+	123231231231231231
+	LMAO 
+	`)
+
+	//	Hands-on Exercise 1
+	fmt.Println("Printing emojisss 😢")
+
+	fmt.Println(`Raw string literals babyyyy
+Hello`)
+}

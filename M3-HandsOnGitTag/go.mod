84a366a5ddeb42d99e8dae27485a04dda2ea0a4c
Thomas Yau <yaucp@connect.hku.hk>
2023-08-06 20:06:08 +0800

    delete

--- M3-HandsOnGitTag/go.mod
+++
@@ -1,3 +0,0 @@
-module M3-HandsOnGitTag
-
-go 1.20
--- M3-HandsOnGitTag/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("This module is used to demonstrate different git tag functions.")
-	fmt.Println("hi 123")
-	fmt.Println()
-}

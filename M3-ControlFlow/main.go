8338f7f4e5079af304905132d3e413a28aceb5cb
Thomas Yau <yaucp@connect.hku.hk>
2023-08-11 13:29:53 +0800

    add for loop & range

--- M3-ControlFlow/main.go
+++ M3-ControlFlow/main.go
@@ -15,21 +15,21 @@ func init() {
 func main() {
 
 	c := 42
-//	If statements
+	//	If statements
 	if c > 43 {
 		fmt.Println("Hey this is impossible!")
 	} else {
 		fmt.Println("Hello this is the proper way!")
 	}
 
-//	statement; statement idiom
-//	scope of y is only limited here (in if-block)
+	//	statement; statement idiom
+	//	scope of y is only limited here (in if-block)
 	if y := 2 * rand.Intn(c); y > 10 {
 		fmt.Printf("y's randomly generated num (%v max) is larger than 10\n", c)
 	}
 
 	num := rand.Intn(200)
-//	switch statements
+	//	switch statements
 	switch {
 	case num > 150:
 		fmt.Println("Larger than 150 damn!")
@@ -39,7 +39,7 @@ func main() {
 	default:
 		fmt.Println("rest of the cases are here....")
 	}
-//	OR
+	//	OR
 	switch num {
 	case 100:
 		fmt.Println(100)
@@ -49,5 +49,33 @@ func main() {
 	default:
 		fmt.Println("rest")
 	}
-//	Add fallthrough key word to run the next case ....
+	//	Add fallthrough key word to run the next case ....
+
+	//	For loop
+	//	Refer to effective go
+	// Basic C-like for loop:
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	//	for = while (inf. loop just put for)
+	for c < 45 {
+		fmt.Println(c)
+		c++
+	}
+
+	//	for range loops
+	xi := []int{41, 42, 4, 1, 100}
+	for idx, val := range xi {
+		fmt.Printf("%v index %v num\n", idx, val)
+	}
+	//	map
+	m := map[string]int{
+		"one": 1,
+		"two": 2,
+	}
+	//Map DONT return things in order!
+	for key, val := range m {
+		fmt.Printf("%v key %v val\n", key, val)
+	}
 }
